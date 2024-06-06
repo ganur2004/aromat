80b3d49cf4cefab1e8e2e05289b3f0bf96f5a817
Gabdollaev Nurmat <aromat-nurmat@server.heavenperfumes.kz>
2024-06-06 18:02:01 +0000

    Changes

--- Aromat/settings.py
+++ Aromat/settings.py
@@ -129,7 +129,10 @@ STATICFILES_DIRS = [
      BASE_DIR / "report/static",
  ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
