3b9eaaf34cd0a72992f3c662daf69a71f578b17b
Gabdollaev Nurmat <aromat-nurmat@server.heavenperfumes.kz>
2024-06-07 06:09:47 +0000

    Changes in secret_key

--- Aromat/settings.py
+++ Aromat/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-k1xy@b0q9f+xp0kcun7*opvmtscz4*x5=l-nd61so#^=!z9npq'
+#SECRET_KEY
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
